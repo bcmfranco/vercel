d1ff31d6343dab0c0b4c94811cf3f8370a05e7b1
bcmfranco <bcmfranco@gmail.com>
2022-05-19 10:48:36 -0300

    add common and add head

---
+++ api/common.php
@@ -0,0 +1,17 @@
+<?php
+
+// SETELITES LOCATION
+
+const SATELITES =  array( // Posición de los satélites
+	'KENOBI'=>[-500, -200],
+	'SKYWALKER'=>[100, -100],
+    'SATO'=>[500, 100]
+);
+
+// AUTH LIST
+const AUTH_LIST =  array( // Perfiles de autorización para cada servicio
+	'welcome'=>[],
+	'locations'=>[],
+	'location'=>['superuser'],
+    'message'=>['admin']
+);
