5e35939099111537700220cf0ae9cbe1e72b903f
bcmfranco <bcmfranco@gmail.com>
2022-05-19 11:00:41 -0300

    delete head and trow head into index

--- api/index.php
+++ api/index.php
@@ -1,7 +1,19 @@
 <?php
     include_once("head.php");
 ?>
-
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="components/mootools/MooTools-Core-1.6.0.js"></script>
+    <script src="components/mootools/MooTools-More-1.6.0.js"></script>
+    <link rel="stylesheet" href="components/styles.css">
+    <title>Star Wars ApiRest</title>
+</head>
+<body>
+    
 <h1>STAR WARS API REST</h1>
 
 <!----------------- WIZARD 1 ------------>
